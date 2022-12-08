e7736f3706c17854f28568d733e1c189eab6034e
Jonathan Kelly <jdk5115@gmail.com>
2022-12-08 16:29:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,12 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-# flatten json 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# wturn json into df
-streamlit.dataframe(fruityvice_normalized)
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# flatten json 
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# wturn json into df
+streamlit.dataframe(fruityvice_normalized)
